9f1d36f54eee0bd4e255a848078f3677fe3fdd3e
Kevin Lai <kevin.km.lai@gmail.com>
2018-11-28 17:03:21 +1100

    added styled-components

--- src/Modal.js
+++ src/Modal.js
@@ -9,8 +9,11 @@ export default class Modal extends Component {
       <Portal>
         {on && (
           <ModelWrapper>
-            <button onClick={toggle}>close</button>
-            <div>{children}</div>
+            <ModalCard>
+              <CloseButton onClick={toggle}>close</CloseButton>
+              <div>{children}</div>
+            </ModalCard>
+            <Background onClick={toggle} />
           </ModelWrapper>
         )}
       </Portal>
@@ -24,5 +27,39 @@ const ModelWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: teal;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  background: white;
+  border-radius: 5px;
+  padding: 15px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+  z-index: 10;
+  min-width: 320px;
+  margin-bottom: 100px;
+`;
+
+// const ModalWindow = styled.div`
+//   position: relative;
+// `;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: black;
+  opacity: 0.5;
 `;
