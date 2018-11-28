564360cab282285aa73c55f886e286befedd4f15
Kevin Lai <kevin.km.lai@gmail.com>
2018-11-28 14:25:43 +1100

    render props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
-import Toggle from "./Toggle";
+import Toggle from "./ToggleRenderProps";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Toggle />
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <h1>I love banana</h1>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        />
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <nav>nav item</nav>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        />
       </div>
     );
   }
